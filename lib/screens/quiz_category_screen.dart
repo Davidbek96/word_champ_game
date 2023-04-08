8321aa07fd0ababd8d6d987982878c1bfc3cab98
David Home <luckystudent96.com>
2023-04-08 17:08:03 +0900

    quiz category changes

--- lib/screens/quiz_category_screen.dart
+++ lib/screens/quiz_category_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:riddle_leader/constants/style_constants.dart';
 
 import '../widgets/gradient_icon_mask.dart';
@@ -10,8 +9,8 @@ class QuizCategoryScreen extends StatelessWidget {
   final int levelNumber;
   @override
   Widget build(BuildContext context) {
-    int _colorIndex;
-    _colorIndex = ((levelNumber - 1) % kGradientColors.length).toInt();
+    int colorIndex;
+    colorIndex = ((levelNumber - 1) % kGradientColors.length).toInt();
 
     return Scaffold(
       body: SafeArea(
@@ -20,51 +19,93 @@ class QuizCategoryScreen extends StatelessWidget {
             GoBackBtnAndTitle(
                 icon: Icons.arrow_back, title: 'Level $levelNumber'),
             Expanded(
-              child: GridView.builder(
-                padding: const EdgeInsets.all(20),
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  childAspectRatio: 1.2,
-                  maxCrossAxisExtent: 150,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15,
+              child: GridView.count(
+                crossAxisCount:2,
+                children: [
+                  
+                ],
                 ),
-                itemCount: 8, //TODO-levels.length change,
-                itemBuilder: (BuildContext ctx, index) {
-                  return GestureDetector(
-                    onTap: () {},
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      color: const Color(0xFF27275E),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          GradientIconMask(
-                            icon: Icon(
-                              kQuizCategoryIcons[index],
-                              //Don't change this color, it is under gradient
-                              color: Colors.white,
-                              size: 50,
-                            ),
-                            colors: [
-                              Colors.white,
-                              kGradientColors[_colorIndex][0]
-                            ],
-                          ),
-                          Text(
-                            kCategoryLabels[index],
-                            style: kLabelStyle,
-                          )
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ListViewCategory extends StatelessWidget {
+  const ListViewCategory({
+    super.key,
+    required this.colorIndex,
+  });
+
+  final int colorIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(10),
+      itemCount: 4,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: Icon(Icons.auto_awesome_motion),
+          tileColor: kGradientColors[colorIndex][0],
+          title: Text('Multiple choice'),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) =>
+          const Divider(),
+    );
+  }
+}
+
+class GridViewCategory extends StatelessWidget {
+  const GridViewCategory({
+    super.key,
+    required this.colorIndex,
+  });
+
+  final int colorIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(20),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        childAspectRatio: 1.2,
+        maxCrossAxisExtent: 150,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15,
+      ),
+      itemCount: 8, //TODO-levels.length change,
+      itemBuilder: (BuildContext ctx, index) {
+        return GestureDetector(
+          onTap: () {},
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            color: const Color(0xFF27275E),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GradientIconMask(
+                  icon: Icon(
+                    kQuizCategoryIcons[index],
+                    //Don't change this color, it is under gradient
+                    color: Colors.white,
+                    size: 50,
+                  ),
+                  colors: [Colors.white, kGradientColors[colorIndex][0]],
+                ),
+                Text(
+                  kCategoryLabels[index],
+                  style: kLabelStyle,
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
