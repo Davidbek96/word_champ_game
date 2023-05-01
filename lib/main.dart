6eb6d02b66105acbe04bdebfb22fc4a6e892ae3c
David Home <luckystudent96.com>
2023-05-02 01:31:03 +0900

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'controllers/user_data_controller.dart';
 import 'screens/login_screen.dart';
 import 'themes/myapp_theme.dart';
 
-void main() {
-   Get.put(UserDataGetx()); //injecting controller in memory
-
+void main() async {
+  Get.put(UserDataGetx()); //injecting controller in memory
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
