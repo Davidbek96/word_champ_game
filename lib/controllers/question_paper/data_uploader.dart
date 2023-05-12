440bd121fc402ade3cff9ee5445fa45f60dafb03
David Home <luckystudent96.com>
2023-05-12 22:06:06 +0900

    still working on firebase connection

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,6 +1,11 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:riddle_leader/firebase_ref/references.dart';
+
+import '../../model/question_paper_model.dart';
 
 //============= Storing file path and name =============
 
@@ -13,6 +18,7 @@ class DataUploaderController extends GetxController {
   }
 
   Future<void> dataUpload() async {
+    final fireStore = FirebaseFirestore.instance;
     //Getting access in assets folder and storing the data into manifestContent variable
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
         .loadString("AssetManifest.json");
@@ -21,12 +27,38 @@ class DataUploaderController extends GetxController {
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
     //Choosing a folder in assets folder and choosing only the .json files and converting map to list
     // and storing the file in paperInAssets variable and file type will be List<String>
-    //
+    // in short , we are getting the path of files with their names
     final paperInAssets = manifestMap.keys
         .where((path) =>
             path.startsWith("assets/DB/paper") && path.contains(".json"))
         .toList();
+    //creating empty List to keep question papers
+    List<QuestionPaperModel> questionPapers = [];
 
-    print(paperInAssets);
+    for (var paper in paperInAssets) {
+      String StringPaperContent = await rootBundle.loadString(paper);
+
+      //adding files as a list (bilogy, chemesitry, ... papers)
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(StringPaperContent)));
+    }
+
+    var batch = fireStore.batch();
+
+    for (var paper in questionPapers) {
+      batch.set(
+        questionPaperRF.doc(paper.id),
+        {
+          "title": paper.title,
+          "image_url": paper.imageUrl,
+          "description": paper.description,
+          "time_seconds": paper.timeSeconds,
+          //questions can be null , so we are checking if it is null
+          "question_count":
+              paper.questions == null ? 0 : paper.questions!.length,
+        },
+      );
+    }
+    await batch.commit();
   }
 }
